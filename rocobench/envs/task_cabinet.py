b7399da3a609b9223b5eecd72e83a2a9da481933
ziyezhishi <ziyezhishi@sjtu.edu.cn>
2026-05-20 12:22:34 +0800

    chore: stabilize cabinet pre-place motion

--- rocobench/envs/task_cabinet.py
+++ rocobench/envs/task_cabinet.py
@@ -154,6 +154,10 @@ class CabinetTask(MujocoSimEnv):
         )
         self.cabinet_pos = self.physics.data.body("cabinet").xpos.copy()
 
+    @property
+    def use_preplace(self):
+        return True
+
     def get_allowed_collision_pairs(self) -> List[Tuple[int, int]]:
         ret = []
         cabinet_ids = self.get_all_body_ids('cabinet') 
